baa341d81b7fdeafd07350244d2729533362f2c2
Ann-sophieH <herman.annsophie@gmail.com>
2022-02-21 16:32:40 +0100

    les 1

---
+++ database/seeders/RolesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('roles')->insert(['name' => 'administrator']);
+        DB::table('roles')->insert(['name' => 'author']);
+        DB::table('roles')->insert(['name' => 'subscriber']);
+    }
+}
